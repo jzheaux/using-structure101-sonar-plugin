bda6f3679e7aaf17dce1a02d2d4103ec342b0d14
Josh Cummings <josh.cummings@gmail.com>
2019-06-25 16:29:31 -0600

    First Commit

---
+++ src/main/java/com/joshcummings/usingstructure101sonarplugin/UsingStructure101SonarPluginApplication.java
@@ -0,0 +1,13 @@
+package com.joshcummings.usingstructure101sonarplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingStructure101SonarPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingStructure101SonarPluginApplication.class, args);
+	}
+
+}
